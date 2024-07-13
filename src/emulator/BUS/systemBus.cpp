64d40d0c181eb99308d318d75364078a9bd72256
710lucas <lucas.peronico@dcx.ufpb.br>
2024-07-13 08:55:26 -0300

    cleaning code

--- src/emulator/BUS/systemBus.cpp
+++ src/emulator/BUS/systemBus.cpp
@@ -54,15 +54,9 @@ void SystemBus::execute(){
     byte control = this->readControl();
 
     if(control >= MEMSTART && control <= MEMEND){
-        // std::cout<<"Memory control signal: "<<control<<std::endl;
-        // if(memoryModule == NULL)
-        //     std::cerr<<"Memory module not set\n";   
-
-        // std::cout<<"Memory module"<<memoryModule<<std::endl;
-        // this->memoryModule->execute(control, address, data);
         if(memoryModule == NULL) {
             std::cerr << "Memory module not set\n";
-            return; // Adiciona esta linha para retornar imediatamente
+            return;
         }
 
         memoryModule->execute(control, address, data);
